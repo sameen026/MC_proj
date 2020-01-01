733f5923e410ca956803f2b6cf84d924361ea7c6
hafizumarbarlas <59201971+hafizumarbarlas@users.noreply.github.com>
2020-01-02 00:24:44 +0500

    Ev bs check kr rha hu k ap k account py hi push ho rha

--- app/src/main/java/com/example/myapplication/DBTest.java
+++ app/src/main/java/com/example/myapplication/DBTest.java
@@ -27,6 +27,7 @@ public class DBTest extends AppCompatActivity implements View.OnClickListener {
     public void onClick(View view) {
         String id=myDB.push().getKey();
         User usr=new User(id,"abc@gmail.com","sameen","abc","membership");
+        //jkkjhk
         myDB.child(id).setValue(usr);
     }
 }
